918a6eda1c7a678c72a1ee8bbd64c41c80719428
Savitha <gsavitha@protonmail.com>
2025-06-29 21:56:36 +0530

    Add basic toutes

--- src/app.js
+++ src/app.js
@@ -2,9 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-  res.send("Hello from server")
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Savitha", lastName: "Gollamudi" })
 })
+app.post("/user", (req, res) => {
+  res.send("User created Succesfully");
+})
+app.put("/user", (req, res) => {
+  res.send("User Updated Succesfully");
+})
+app.patch("/user", (req, res) => {
+  res.send("Portion of a user got updated succesfully");
+})
+app.delete("/user", (req, res) => {
+  res.send("User has been deleted Succesfully");
+})
+app.use("/test", (req, res) => {
+  res.send("Hello from Server")
+})
+
+
+
 
 app.listen(7777, () => {
   console.log('Server is listening on port 3000');
